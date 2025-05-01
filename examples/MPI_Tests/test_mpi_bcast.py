75e07cde5195accdcb937a90cdca570dc9af45db
austin-hoover <austin.michael.hoover@gmail.com>
2025-05-01 12:30:12 -0400

    Edit test_mpi_bcast.py

--- examples/MPI_Tests/test_mpi_bcast.py
+++ examples/MPI_Tests/test_mpi_bcast.py
@@ -4,19 +4,19 @@ mpi_comm = orbit_mpi.mpi_comm.MPI_COMM_WORLD
 mpi_rank = orbit_mpi.MPI_Comm_rank(mpi_comm)
 mpi_size = orbit_mpi.MPI_Comm_size(mpi_comm)
 
+main_rank = 0
+
 data = (1, 2, 3)
 data_type = orbit_mpi.mpi_datatype.MPI_INT
-main_rank = 0
 data_new = orbit_mpi.MPI_Bcast(data, data_type, main_rank, mpi_comm)
 print(f"rank={mpi_rank} data_new={data_new}")
 
 data = (1.0, 2.0, 3.0)
 data_type = orbit_mpi.mpi_datatype.MPI_DOUBLE
-main_rank = 0
 data_new = orbit_mpi.MPI_Bcast(data, data_type, main_rank, mpi_comm)
 print(f"rank={mpi_rank} data_new={data_new}")
 
 data = "test"
 data_type = orbit_mpi.mpi_datatype.MPI_CHAR
-main_rank = 0
 data_new = orbit_mpi.MPI_Bcast(data, data_type, main_rank, mpi_comm)
+print(f"rank={mpi_rank} data_new={data_new}")
